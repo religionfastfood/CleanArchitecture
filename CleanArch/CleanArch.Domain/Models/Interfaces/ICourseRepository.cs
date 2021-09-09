3488bca1222be6323e604c45e448d0e58ec7eb86
Chris McDonald <christian.mcdonald90@gmail.com>
2021-09-09 11:23:20 -0400

    Commit Course Service and Repository Interfaces, CourseViewModel

---
+++ CleanArch/CleanArch.Domain/Models/Interfaces/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Models.Interfaces
+{
+	public interface ICourseRepository
+	{
+		IEnumerable<Course> GetCourses();
+	}
+}
